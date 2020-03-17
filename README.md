4ff598cca436a3bb1669f4536e928041ca167cca
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-03-17 23:39:05 +0100

    Create barebone project

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  <h1 align="center">PyCran</h1>
+</p>
---
+++ pycran/__init__.py
@@ -0,0 +1,110 @@
+from typing import Dict, Generator, Optional, Union
+
+
+BytesOrString = Union[bytes, str]
+
+
+def as_string(meta_line: BytesOrString) -> str:
+    if isinstance(meta_line, bytes):
+        return meta_line.decode("utf-8")
+
+    return meta_line
+
+
+def from_packages_list(data: BytesOrString) -> Generator:
+    """Parses CRAN package metadata from
+    https://cran.r-project.org/src/contrib/PACKAGES
+    and returns the list of dictionaries.
+    Args:
+        data (BytesOrString): raw text from the package list
+    Returns:
+        (Generator): each entry from packages as dictionary
+    """
+    fields = set()
+    package = {}
+
+    # We want to iterate over each line and accumulate
+    # keys in dictionary, once we meet the same key
+    # in our dictionary we have a single package
+    # metadata parsed so we yield and repeat again.
+    for line in data.splitlines():
+        line = as_string(line)
+
+        if not line.strip():
+            continue
+
+        if ":" in line:
+            parts = line.split(":")
+            field = str(parts[0].strip())
+            value = str("".join(parts[1:]).strip())
+
+            if field in fields:
+                fields = {field}
+                result = {**package}
+                package = {field: value}
+                if result:
+                    yield result
+            else:
+                # Here we want to parse dangling lines
+                # like the ones with long dependency
+                # list, `R (>= 2.15.0), xtable, pbapply ... \n    and more`
+                package[field] = str(value)
+                fields.add(field)
+        else:
+            pairs = list(package.items())
+            if pairs:
+                last_field = str(pairs[-1][0])
+                package[last_field] += f" {line.strip()}"
+
+    # We also need to return the metadata for
+    # the last parsed package.
+    if package:
+        yield package
+
+
+def to_cran_format(metadata: Dict) -> Optional[str]:
+    """
+    Dump dictionary into the following form
+        Package: A3
+        Version: 1.0.0
+        Depends: R (>= 2.15.0), xtable, pbapply
+        Suggests: randomForest, e1071
+        License: GPL (>= 2)
+        MD5sum: 027ebdd8affce8f0effaecfcd5f5ade2
+        NeedsCompilation: no
+
+    Args:
+        metadata (Dict): Converts metadata dictionary to deb format
+    Returns:
+        (Optional[str]): package record as deb format
+    """
+    return "\n".join([
+        f"{key}: {value}"
+        for key, value in metadata.items()
+    ])
+
+
+def from_cran_format(metadata: str) -> Optional[Dict]:
+    """Parse package metadata
+    Note: it is a shorthand to `from_packages_list`
+          then extracts the first value from it.
+    Input should be in the following format
+    which is R package metadata description
+    see: https://cran.r-project.org/src/contrib/PACKAGES
+        Package: A3
+        Version: 1.0.0
+        Depends: R (>= 2.15.0), xtable, pbapply
+        Suggests: randomForest, e1071
+        License: GPL (>= 2)
+        MD5sum: 027ebdd8affce8f0effaecfcd5f5ade2
+        NeedsCompilation: no
+    Args:
+        metadata (str): metadata text information
+    Returns:
+        (Optional[Dict]): Parse deb format and return dictionary
+    """
+    packages = list(from_packages_list(metadata))
+    if packages:
+        return packages[0]
+
+    return None
