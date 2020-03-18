8f7b6f9da5328dc4ef2ce2678ff5cc8aa44af4e2
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-03-18 11:16:13 +0100

    Check if archive is tarfile

--- pycran/util.py
+++ pycran/util.py
@@ -15,6 +15,9 @@ def as_string(meta_line: BytesOrString) -> str:
 
 def get_description(archive: PathOrTarFile) -> str:
     if isinstance(archive, str):
+        if not tarfile.is_tarfile(archive):
+            raise tarfile.TarError(f"File {archive} is not tar archive")
+
         with tarfile.open(archive) as tar:
             description = tar.getmember("DESCRIPTION")
             with tar.fileobject(tar, description) as metadata:
